61f3e4a08ff02f97461b4b533d20228e2b9c9b0e
Nebukadnezzar13 <hissler.patrick@yahoo.de>
2023-12-10 16:41:21 +0100

    created prism and some classes

---
+++ PMPP/MusicPlayer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+
+namespace PMPP
+{
+    class MusicPlayer
+    {
+        private static MusicPlayer singleton = null;
+        private AudioFileReader audioFile = null;
+        public string actualSong = string.Empty;
+        private WaveOutEvent waveOut;
+
+        private MusicPlayer()
+        {
+
+
+        }
+
+        public static MusicPlayer Instance
+        {
+            get
+            {
+                if (singleton == null)
+                {
+                    singleton = new MusicPlayer();
+                }
+                return singleton;
+            }
+        }
+
+        public void playSong()
+        {
+            if (audioFile != null)
+            {
+                waveOut.Play();
+            }
+        }
+
+        public void pauseSong()
+        {
+            if (audioFile != null)
+            {
+                waveOut.Pause();
+            }
+        }
+
+
+        public void setActualSong(string song)
+        {
+            actualSong = song;
+            if (audioFile == null)
+            {
+                audioFile = new AudioFileReader(actualSong);
+                waveOut = new WaveOutEvent();
+            }
+
+        }
+    }
+}
---
+++ PMPP/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+
+namespace PMPP.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        private MusicPlayer MusicPlayer = MusicPlayer.Instance;
+
+        private DelegateCommand _playCommand;
+        public DelegateCommand PlayCommand =>
+            _playCommand ?? (_playCommand = new DelegateCommand(playCommand));
+
+
+        private void playCommand()
+        {
+            MusicPlayer.playSong();
+        }
+
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
