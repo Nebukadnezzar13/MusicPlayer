4464a76e80997454ce5125963f5cf6bcba58f45d
Nebukadnezzar13 <hissler.patrick@yahoo.de>
2023-12-17 17:07:19 +0100

    several classes created

---
+++ PMPP/Song.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMPP
+{
+    internal class Song
+    {
+       
+        public string album;
+        public string artist;
+        public string title;
+        public string url;
+        public int bitrate;
+        public int duration;
+        public string fileLocation;
+        
+        public Song() { }
+
+
+        public enum source
+        {
+            Local,
+            Spotify,
+            Youtube
+        }
+
+
+
+    }
+}
--- PMPP/obj/Debug/net6.0-windows/App.g.i.cs
+++ PMPP/obj/Debug/net6.0-windows/App.g.i.cs
@@ -66,7 +66,7 @@ namespace PMPP {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/PMPP;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PMPP;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
